f2c9de587bcf27f4d45b19a152984b413619ca08
Leonid Shutov <leonid.shutov@gmail.com>
2024-06-15 17:38:03 -0700

    refactor
    extract input handling. move it to use signals

--- scene/main/main_screen.gd
+++ scene/main/main_screen.gd
@@ -1,19 +1,5 @@
 extends Node2D
 
-
-const MOVE_LEFT: StringName = &"move_left"
-const MOVE_RIGHT: StringName = &"move_right"
-const MOVE_UP: StringName = &"move_up"
-const MOVE_DOWN: StringName = &"move_down"
-
-const MOVE_INPUTS: Array[StringName] = [
-	MOVE_LEFT,
-	MOVE_RIGHT,
-	MOVE_UP,
-	MOVE_DOWN,
-]
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_connect_signals(NodeReference.SIGNAL_CONNECTIONS)
@@ -24,26 +10,6 @@ func _ready() -> void:
 func _create_pc() -> void:
 	SpriteFactory.create_actor(SubTag.PC, Vector2i(0, 0), true)
 
-func _move_pc(direction: StringName) -> void:
-	var pc: Sprite2D = get_tree().get_first_node_in_group(SubTag.PC)
-	var coord: Vector2i = ConvertCoord.get_coord(pc)
-
-	match direction:
-		MOVE_LEFT:
-			coord += Vector2i.LEFT
-		MOVE_RIGHT:
-			coord += Vector2i.RIGHT
-		MOVE_UP:
-			coord += Vector2i.UP
-		MOVE_DOWN:
-			coord += Vector2i.DOWN
-	pc.position = ConvertCoord.get_position(coord)
-
-func _unhandled_input(event: InputEvent) -> void:
-	for i: StringName in MOVE_INPUTS:
-		if event.is_action_pressed(i):
-			_move_pc(i)
-			
 func _connect_signals(signal_connections: Dictionary) -> void:
 	var signals_from_one_node: Dictionary
 	var target_nodes: Array
